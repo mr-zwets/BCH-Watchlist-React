9bdd3c86fa51098f72be4ac880b5abc0b8b0f6a3
Mathieu <mr-zwets@protonmail.com>
2021-07-30 10:13:25 +0200

    state of input and list addresses

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            input:'',
+            watchlist: []
+        };
+        this.addToWatchlist=this.addToWatchlist.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    handleChange=(e) => { this.setState({input: e.target.value}) }
+
+    addToWatchlist=() => {
+        this.setState({
+            input:'',
+            watchlist: [...this.state.watchlist,this.state.input]
+        }) 
+    }
+
+    render(){ return (
+        <div>
+            <h1>BCH Address Watchlist</h1>
+            <div>Add Bitcoin Cash Addresses To Watch:</div>
+            <input type="text" value={this.state.input} onChange={(e) => this.handleChange(e)} placeholder="BCH address"/>
+            <button id="myBtn" onClick={this.addToWatchlist}>Add</button>
+            <section style={{display: "flex"}}>
+                {this.state.watchlist.map(addr =>
+                    (<div key={addr} style={{width: "15em"}}>
+                        <img src="https://cdn.mainnet.cash/wait.svg" style={{width: "inherit"}} id="deposit" />
+                        <div style={{overflow: "auto"}}>{addr}</div>
+                    </div>) 
+                )}
+            </section>
+        </div>
+    )}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
