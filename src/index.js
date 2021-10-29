8e2b0fbd95b6d2698c3d0ed764c7fa84b3ff4a52
Mathieu <mr-zwets@protonmail.com>
2021-10-29 23:54:03 +0200

    added styling with CSS, imported BCH-js & linting

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import BCHJS through browserify
+import BCHJS from "@psf/bch-js";
+let bchjs = new BCHJS(); // Defaults to BCHN network.
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            input:'',
-            watchlist: []
-        };
-        this.addToWatchlist=this.addToWatchlist.bind(this);
-        this.handleChange=this.handleChange.bind(this);
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      watchlist: [],
+    };
+    this.addToWatchlist = this.addToWatchlist.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange=(e) => { this.setState({input: e.target.value}) }
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
+  };
 
-    addToWatchlist=() => {
-        this.setState({
-            input:'',
-            watchlist: [...this.state.watchlist,this.state.input]
-        }) 
-    }
+  addToWatchlist = () => {
+    this.setState({
+      input: "",
+      watchlist: [...this.state.watchlist, this.state.input],
+    });
+  };
 
-    render(){ return (
-        <div>
-            <h1>BCH Address Watchlist</h1>
-            <div>Add Bitcoin Cash Addresses To Watch:</div>
-            <input type="text" value={this.state.input} onChange={(e) => this.handleChange(e)} placeholder="BCH address"/>
-            <button id="myBtn" onClick={this.addToWatchlist}>Add</button>
-            <section style={{display: "flex"}}>
-                {this.state.watchlist.map(addr =>
-                    (<div key={addr} style={{width: "15em"}}>
-                        <img src="https://cdn.mainnet.cash/wait.svg" style={{width: "inherit"}} id="deposit" />
-                        <div style={{overflow: "auto"}}>{addr}</div>
-                    </div>) 
-                )}
-            </section>
+  render() {
+    return (
+      <div>
+        <h1>BCH Watchlist</h1>
+        <div>Enter Bitcoin Cash address to add to watchlist:</div>
+        <div class="container">
+          <input
+            type="text"
+            id="inputAddr"
+            value={this.state.input}
+            onChange={(e) => this.handleChange(e)}
+            placeholder="BCH address"
+          />
+          <button id="myBtn" onClick={this.addToWatchlist}>
+            ADD
+          </button>
         </div>
-    )}
+        <section style={{ display: "flex" }}>
+          {this.state.watchlist.map((addr) => (
+            <div key={addr} style={{ width: "15em" }}>
+              <img
+                src="https://cdn.mainnet.cash/wait.svg"
+                style={{ width: "inherit" }}
+                id="deposit"
+              />
+              <div style={{ overflow: "auto" }}>{addr}</div>
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
