02e8eda80134c5ab085f5bfed10442403160ee4f
Mathieu <mr-zwets@protonmail.com>
2021-11-05 16:32:43 +0100

    fixed bug where having no watchlist in localstorage causes error

--- src/index.js
+++ src/index.js
@@ -1,53 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import BCHJS from "@psf/bch-js";
 import QRCode from "qrcode.react";
 let bchjs = new BCHJS();
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputAddr: "",
-      watchlist: [],
-      BCHprice: 0,
+function App() {
+  const [inputAddr, setInputAddr] = useState("");
+  const [watchlist, setWatchlist] = useState([]);
+  const [BCHprice, setBCHprice] = useState(0);
+
+  useEffect(() => {
+    const fetchPrice = async () => {
+      try {
+        const BCHprice = await bchjs.Price.getBchUsd();
+        setBCHprice(BCHprice);
+      } catch (err) {
+        console.log(err);
+      }
     };
-    this.addToWatchlist = this.addToWatchlist.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.fetchBalance = this.fetchBalance.bind(this);
-    this.removeFromWatchlist = this.removeFromWatchlist.bind(this);
-    this.readLocalStorage = this.readLocalStorage.bind(this);
-    this.writeToLocalStorage = this.writeToLocalStorage.bind(this);
-  }
 
-  componentDidMount = async () => {
-    try {
-      // Read local storage on initialization
-      this.readLocalStorage()
-      // Fetch BCH price on initialization
-      const BCHprice = await bchjs.Price.getBchUsd();
-      this.setState({ BCHprice });
-    } catch (err) {
-      console.log(err);
-    }
+    const readLocalStorage = () => {
+      // Convert the string back to the watchlist object
+      const newWatchlist = JSON.parse(localStorage.getItem("watchlist"));
+      // If the local storage is not null
+      if (newWatchlist !== null) {
+        // Refresh the balances of all addresses
+        newWatchlist.forEach(async (addrObj) => {
+          addrObj.balance = await fetchBalance(addrObj.address);
+        });
+        // Write the watchlist to state, dot notation for rerender
+        setWatchlist([...newWatchlist]);
+      }
+    };
+    // Read local storage on initialization
+    readLocalStorage();
+    // Fetch BCH price on initialization
+    fetchPrice();
+  }, []);
+
+  useEffect(() => {
+    const writeToLocalStorage = () => {
+      // Clear local storage and write the watchlist array to it as a string
+      localStorage.clear();
+      localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    };
+    // Write the watchlist to localStorage each time it changes
+    writeToLocalStorage();
+  }, [watchlist]);
+
+  const handleChange = (e) => {
+    setInputAddr(e.target.value);
   };
 
-  handleChange = (e) => {
-    this.setState({ inputAddr: e.target.value });
+  const changeName = (e, index) => {
+    let newWatchlist = watchlist;
+    newWatchlist[index].name = e.target.value;
+    setWatchlist([...newWatchlist]);
+    // Dot notation so React rerenders
   };
 
-  changeName = (e, index) => {
-    let watchlist = this.state.watchlist;
-    watchlist[index].name = e.target.value;
-    this.setState({ watchlist });
-    this.writeToLocalStorage()
-  };
-
-  addToWatchlist = async () => {
-    const newAddress = this.state.inputAddr;
-    const addrObj = { address: newAddress, name: "" };
-    // Check if address is already on the watchlist 
-    if (this.state.watchlist.includes(newAddress)) {
+  const addToWatchlist = async () => {
+    const newAddress = inputAddr;
+    const newAddrObj = { address: newAddress, name: "", balance: 0 };
+    // Check if address is already on the watchlist
+    if (watchlist.find((addrObj) => addrObj.address === newAddress)) {
       alert("Already added this address to the watchlist!");
       return;
     }
@@ -59,22 +75,19 @@ class App extends React.Component {
       return;
     }
     // Balance denominated in BCH
-    const balance = await this.fetchBalance(newAddress);
-    addrObj.balance = balance;
+    const balance = await fetchBalance(newAddress);
+    newAddrObj.balance = balance;
     // Clear input, add address object to the state
-    this.setState({
-      inputAddr: "",
-      watchlist: [...this.state.watchlist, addrObj],
-    });
-    // Write the watchlist to local storage
-    this.writeToLocalStorage()
+    setInputAddr("");
+    setWatchlist([...watchlist, newAddrObj]);
   };
 
-  fetchBalance = async (newAddress) => {
+  const fetchBalance = async (newAddress) => {
     try {
       // Fetch the BCH balance in satoshis and convert it to BCH denomination
       const response = await bchjs.Electrumx.balance(newAddress);
-      const totalBalance = response.balance.confirmed + response.balance.unconfirmed
+      const totalBalance =
+        response.balance.confirmed + response.balance.unconfirmed;
       const BCHbalance = totalBalance / 100000000;
       return BCHbalance;
     } catch (error) {
@@ -82,105 +95,84 @@ class App extends React.Component {
     }
   };
 
-  removeFromWatchlist = (index) => {
-    let watchlist = this.state.watchlist;
-    watchlist.splice(index, 1);
-    this.setState({ watchlist });
-    // Write the watchlist to local storage
-    this.writeToLocalStorage()
+  const removeFromWatchlist = (index) => {
+    let newWatchlist = watchlist;
+    newWatchlist.splice(index, 1);
+    setWatchlist([...newWatchlist]);
+    // Dot notation so React rerenders
   };
 
-  writeToLocalStorage = () => {
-    // Clear local storage and write the watchlist array to it as a string
-    localStorage.clear();
-    localStorage.setItem("watchlist", JSON.stringify(this.state.watchlist));
-  }
-
-  readLocalStorage = () => {
-    // Convert the string back to the watchlist object
-    let watchlist = JSON.parse(localStorage.getItem("watchlist"));
-    // Refresh the balances of all addresses
-    watchlist.forEach( async (addrObj) => {
-      addrObj.balance = await this.fetchBalance(addrObj.address);
-    })
-    // Write the watchlist to state if it exists
-    if(watchlist !== "null") this.setState({ watchlist });
-  }
-
-  render() {
-    return (
-      <div>
-        <main>
-          <h1>BCH Watchlist</h1>
-          <div>Enter Bitcoin Cash address to add to watchlist:</div>
-          <div className="container">
-            <input
-              type="text"
-              id="inputAddr"
-              value={this.state.inputAddr}
-              onChange={(e) => this.handleChange(e)}
-              onKeyPress= {(event) => {
-                if (event.key === "Enter") this.addToWatchlist();
-              }}
-              placeholder="BCH address"
-            />
-            <button id="myBtn" onClick={this.addToWatchlist}>
-              ADD
-            </button>
-          </div>
-          <section id="addresslist">
-            {this.state.watchlist.map(({ address, balance, name }, index) => (
-              <div key={address} className="item">
-                <input
-                  type="text"
-                  id="inputName"
-                  value={name}
-                  onChange={(e) => this.changeName(e, index)}
-                  className="inputName"
-                  placeholder="name"
-                />
-                <div id="x" onClick={() => this.removeFromWatchlist(index)}>
-                  X
-                </div>
-                <QRCode
-                  id="QR"
-                  value={address}
-                  size={240}
-                  renderAs="svg"
-                  includeMargin={true}
-                />
-                <div id="addr">{address}</div>
-                <br />
-                <div>
-                  {" "}
-                  balance: {balance} BCH (
-                  {(balance * this.state.BCHprice).toFixed(2)}
-                  $)
-                </div>
+  return (
+    <div>
+      <main>
+        <h1>BCH Watchlist</h1>
+        <div>Enter Bitcoin Cash address to add to watchlist:</div>
+        <div className="container">
+          <input
+            type="text"
+            id="inputAddr"
+            value={inputAddr}
+            onChange={(e) => handleChange(e)}
+            onKeyPress={(event) => {
+              if (event.key === "Enter") addToWatchlist();
+            }}
+            placeholder="BCH address"
+          />
+          <button id="myBtn" onClick={addToWatchlist}>
+            ADD
+          </button>
+        </div>
+        <section id="addresslist">
+          {watchlist.map(({ address, balance, name }, index) => (
+            <div key={address} className="item">
+              <input
+                type="text"
+                id="inputName"
+                value={name}
+                onChange={(e) => changeName(e, index)}
+                className="inputName"
+                placeholder="name"
+              />
+              <div id="x" onClick={() => removeFromWatchlist(index)}>
+                X
               </div>
-            ))}
-          </section>
-        </main>
-        <footer>
+              <QRCode
+                id="QR"
+                value={address}
+                size={240}
+                renderAs="svg"
+                includeMargin={true}
+              />
+              <div id="addr">{address}</div>
+              <br />
+              <div>
+                {" "}
+                balance: {balance} BCH ({(balance * BCHprice).toFixed(2)}
+                $)
+              </div>
+            </div>
+          ))}
+        </section>
+      </main>
+      <footer>
+        <a
+          href="https://github.com/mr-zwets/BCH-Watchlist-React"
+          target="_blanc"
+        >
+          <img alt="" className="githublogo" src="./github.png" />
+        </a>
+        <p>
+          built with{" "}
           <a
-            href="https://github.com/mr-zwets/BCH-Watchlist-React"
+            href="https://github.com/Permissionless-Software-Foundation/bch-js"
             target="_blanc"
           >
-            <img alt="" className="githublogo" src="./github.png" />
+            bch-js
           </a>
-          <p>
-            built with{" "}
-            <a
-              href="https://github.com/Permissionless-Software-Foundation/bch-js"
-              target="_blanc"
-            >
-              bch-js
-            </a>
-          </p>
-        </footer>
-      </div>
-    );
-  }
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
